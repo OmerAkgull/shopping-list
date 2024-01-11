7ca8d0382d1de4865a872ffd8e994a6d6458553f
OmerAkgull <143122832+OmerAkgull@users.noreply.github.com>
2024-01-11 19:46:18 +0300

    Create function to add products on click

--- src/components/inputGroup.jsx
+++ src/components/inputGroup.jsx
@@ -1,48 +1,67 @@
+import { useState } from "react";
 import { Form } from "react-bootstrap";
 import { InputGroup } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 
 const shops = [
-    { id: 1, name: "Hepsiburada" },
-    { id: 2, name: "Trendyol" },
-    { id: 3, name: "n11" },
-    { id: 4, name: "Migros" },
-    { id: 5, name: "Carrefour" },
-  ];
+  { id: 1, name: "Hepsiburada" },
+  { id: 2, name: "Trendyol" },
+  { id: 3, name: "n11" },
+  { id: 4, name: "Migros" },
+  { id: 5, name: "Carrefour" },
+];
 
-  const categories = [
-    { id: 1, name: "Electronics" },
-    { id: 2, name: "Cosmetics" },
-    { id: 3, name: "Books" },
-    { id: 4, name: "Sports Equipment" },
-    { id: 5, name: "Clothing" },
-  ];
+const categories = [
+  { id: 1, name: "Electronics" },
+  { id: 2, name: "Cosmetics" },
+  { id: 3, name: "Books" },
+  { id: 4, name: "Sports Equipment" },
+  { id: 5, name: "Clothing" },
+];
 
-  const shopOptions = shops.map((shop) => <option value={shop.id} key={shop.id}>{shop.name}</option>)
-  const categoryOptions = categories.map((category) => <option value={category.id} key={category.id}>{category.name}</option>)
+const shopOptions = shops.map((shop) => (
+  <option value={shop.id} key={shop.id}>
+    {shop.name}
+  </option>
+));
+const categoryOptions = categories.map((category) => (
+  <option value={category.id} key={category.id}>
+    {category.name}
+  </option>
+));
 
 export default function MyInputGroup() {
-    
+  const [productInput, setProductInput] = useState("");
+  const [product, setProduct] = useState("");
+
+  function addProduct(e) {
+    e.preventDefault();
+    setProduct([...product, productInput]);
+    setProductInput("");
+  }
+
   return (
-      <InputGroup className="mb-3">
-        <InputGroup.Text id="inputGroup-sizing-default">
-          Product Name
-        </InputGroup.Text>
-        <Form.Control
-          aria-label="Default"
-          aria-describedby="inputGroup-sizing-default"
-        />
-        <Form.Select>
-          <option>Select Shop</option>
-          {shopOptions}
-        </Form.Select>
-        <Form.Select>
-          <option>Select Category</option>
-          {categoryOptions}
-        </Form.Select>
-        <Button variant="primary" type="submit">
+    <InputGroup className="mb-3">
+      <InputGroup.Text id="inputGroup-sizing-default">
+        Product Name
+      </InputGroup.Text>
+      <Form.Control
+        onChange={(e) => setProductInput(e.target.value)}
+        value={productInput}
+        aria-label="Default"
+        aria-describedby="inputGroup-sizing-default"
+      />
+      <Form.Select>
+        <option>Select Shop</option>
+        {shopOptions}
+      </Form.Select>
+      <Form.Select>
+        <option>Select Category</option>
+        {categoryOptions}
+      </Form.Select>
+      <Button onClick={addProduct} variant="primary" type="submit">
         Add
       </Button>
-      </InputGroup>
+    </InputGroup>
   );
 }
